f444927b110c9b36432c81391cf5f1ac10021bb2
Robert Jones <rob@redshirtsoftware.com>
2013-03-30 19:01:43 -0700

    Initial Commit

---
+++ Morpheme/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Morpheme
+//
+//  Created by Robert Jones on 3/30/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
